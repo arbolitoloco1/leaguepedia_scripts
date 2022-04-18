b49fda876a9cca3b2690a58cad49b7194fffe901
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-04-17 22:29:33 -0300

    Update autorosters.py

--- autorosters/autorosters.py
+++ autorosters/autorosters.py
@@ -1,6 +1,7 @@
 from mwrogue.esports_client import EsportsClient
 from rivercogutils import utils
 from redbot.core import commands
+from asyncio import TimeoutError
 
 from autorosters.autorosters_main import AutoRostersRunner
 
@@ -12,11 +13,18 @@ class AutoRosters(commands.Cog):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def autorosters(self, ctx, overview_page):
+    async def autorosters(self, ctx, *, overview_page):
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+        await ctx.send("Tabs:")
+        try:
+            tabs = await self.bot.wait_for("message", check=check, timeout=60)
+        except TimeoutError:
+            return
         await ctx.send('Okay, starting now!')
         credentials = await utils.get_credentials(ctx, self.bot)
         site = EsportsClient('lol', credentials=credentials,
                              max_retries_mwc=0,
                              max_retries=2, retry_interval=10)
-        AutoRostersRunner(site, overview_page).run()
+        AutoRostersRunner(site, overview_page, tabs).run()
         await ctx.send('Okay, done!')

36c6a2b7d661a010c89e749f7ee03d6377a4040f
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-04-19 17:04:00 -0300

    Update autorosters.py

--- autorosters/autorosters.py
+++ autorosters/autorosters.py
@@ -7,8 +7,7 @@ from .autorosters_main import AutoRostersRunner
 
 
 async def is_lol_staff(ctx) -> bool:
-    for role in ctx.message.guild.roles:
-        staff_role = role break if role.name == "LoL-Staff"
+    staff_role = lambda role: role.name == 'LoL-Staff', ctx.message.guild.roles
     if staff_role not in ctx.author.roles:
         raise commands.UserFeedbackCheckFailure("You don't have enough permissions to run this command!")
     return True

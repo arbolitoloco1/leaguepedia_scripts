6fd46d74fadd396545f110f9d6b6f34aeacaecfe
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-04-20 17:33:00 -0300

    Update autorosters.py

--- autorosters/autorosters.py
+++ autorosters/autorosters.py
@@ -26,17 +26,10 @@ class AutoRosters(commands.Cog):
     @commands.check(is_lol_staff)
     async def autorosters(self, ctx, *, overview_page):
         """Generate team rosters for the specified tournament"""
-        def check(msg):
-            return msg.author == ctx.author and msg.channel == ctx.channel
-        await ctx.send("Tabs: (Eg. `LEC 2022`)")
-        try:
-            tabs = await self.bot.wait_for("message", check=check, timeout=60)
-        except TimeoutError:
-            return
         await ctx.send('Okay, starting now!')
         credentials = await utils.get_credentials(ctx, self.bot)
         site = EsportsClient('lol', credentials=credentials,
                              max_retries_mwc=0,
                              max_retries=2, retry_interval=10)
-        AutoRostersRunner(site, overview_page, tabs.content).run()
+        AutoRostersRunner(site, overview_page).run()
         await ctx.send('Okay, done!')

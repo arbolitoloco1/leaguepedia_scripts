c368ec630166f957e5a287f7bd27a12c174d52b6
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-04-20 23:24:43 -0300

    Update autorosters.py

--- autorosters/autorosters.py
+++ autorosters/autorosters.py
@@ -34,6 +34,7 @@ class AutoRosters(commands.Cog):
         overview_page = site.cache.get_target(overview_page)
         if not overview_page:
             return await ctx.send('The tournament page does not exist!')
+        await ctx.send(str(overview_page))
         AutoRostersRunner(site, overview_page).run()
         sandbox_page = f"User:{site.credentials.username.split('@')[0]}/Team Rosters Sandbox"
         rosters_page = f"{overview_page}/Team Rosters"

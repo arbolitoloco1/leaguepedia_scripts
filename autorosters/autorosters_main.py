dce2f5baf89bd7b517302d71c17ee27718b85fcf
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-04-21 15:29:04 -0300

    Update autorosters_main.py

--- autorosters/autorosters_main.py
+++ autorosters/autorosters_main.py
@@ -198,6 +198,7 @@ class AutoRostersRunner(object):
             for game in match["games"].values():
                 for team in current_teams:
                     rd_team = self.rosters_data[team]
+                    print(rd_team)
                     for player in rd_team["players"].keys():
                         game_rd_player = rd_team["players"][player]
                         if "sg_data" in game.keys():
